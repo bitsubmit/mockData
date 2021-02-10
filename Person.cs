ceb6c9d3d4f5296d3102fc962c0f3724503ed332
Simon Jakobsen <simonjakobsen9@hotmail.com>
2021-02-10 15:51:58 +0100

    init

---
+++ Person.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace MockData.Model
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Person> Persons = new List<Person>(); 
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using MockData.Model;
+using System;
+using System.Collections.Generic;
+
+namespace MockData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> persons = new List<Person>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                var person = new Person
+                {
+                    Id = i,
+                    Name = i.ToString()
+                };
+
+                persons.Add(person);
+            }
+
+            foreach (Person person in persons)
+            {
+                Console.WriteLine(person.Id);
+            }
+        }
+    }
+}
